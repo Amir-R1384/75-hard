dbb0f076353e17a27b700c6b87e283815587c066
Amir Ranjbar <ranjbaramirreza5786@yahoo.com>
2023-05-14 22:07:56 -0400

    Minor UI fix

--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -10,7 +10,7 @@ export default function PlusButton({ onClick, inverse = false }: Props) {
 	return (
 		<Button
 			onClick={onClick}
-			className="border p-0.5 w-7 bg-gray-100 border-dark rounded-[10px]">
+			className="border p-0.5 w-7 stroke-dark bg-gray-100 border-dark rounded-[10px]">
 			{inverse ? <MinusIcon /> : <PlusIcon />}
 		</Button>
 	)
